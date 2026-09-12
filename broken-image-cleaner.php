e739d54681dc63a3288805ae3cef1c26e7cee10a
Harit Panchal <panchalharit007@gmail.com>
2026-09-12 22:44:11 +0530

    Link the author name to the wordpress.org profile
    The plugin row showed the author as plain text because the header carried no
    Author URI for it to link to.

--- broken-image-cleaner.php
+++ broken-image-cleaner.php
@@ -7,6 +7,7 @@
  * Requires at least: 5.6
  * Requires PHP:      7.4
  * Author:            Harit Panchal
+ * Author URI:        https://profiles.wordpress.org/haritpanchal/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       broken-image-cleaner
